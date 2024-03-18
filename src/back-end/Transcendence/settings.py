bc57ebd1936de72ca93ca806f3579c084584983b
kyaubry <aubry.kylian973@gmail.com>
2024-03-18 18:47:44 +0100

    merge

--- src/back-end/Transcendence/settings.py
+++ src/back-end/Transcendence/settings.py
@@ -51,6 +51,10 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
+    'http://localhost:8000',
+    'http://127.0.0.1:443',
+    'http://localhost:443',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
