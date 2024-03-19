7171a05dc698301ff4151d37eddbd4cb107dbc65
kyaubry <aubry.kylian973@gmail.com>
2024-03-19 17:31:12 +0100

    push

--- src/back-end/users/models.py
+++ src/back-end/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
 
@@ -29,7 +29,7 @@ class AppUserManager(BaseUserManager):
 		user.save(using=self._db)
 		return user
 
-class appuser(AbstractUser, PermissionsMixin):
+class appuser(AbstractBaseUser, PermissionsMixin):
 	user_id = models.AutoField(primary_key=True)
 	email = models.EmailField(max_length=50, unique=True)
 	username = models.CharField(max_length=50)
