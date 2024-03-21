dec3876f5d4a3c0a89c8476f271e75d360aa0dc8
Donovan Grandin <dgrandin@c2r7p1.42lehavre.fr>
2024-03-21 12:54:31 +0100

    update apps.py

--- src/back-end/Transcendence/settings.py
+++ src/back-end/Transcendence/settings.py
@@ -100,6 +100,13 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
