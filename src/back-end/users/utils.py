9aa0f5ab34971301c7bd54e6eac62aee64070d6a
Donovan Grandin <dgrandin@c2r1p3.42lehavre.fr>
2024-03-23 18:48:40 +0100

    test

--- src/back-end/users/utils.py
+++ src/back-end/users/utils.py
@@ -34,3 +34,33 @@ def updateUserOption(user_id, language, color, music, key1, key2, key3, key4):
     user.key4 = key4
 
     user.save()
+
+from django.conf import settings
+def ManageGameQueue():
+    if (settings.IS_SEARCHING == False):
+        settings.IS_SEARCHING = True
+        from . models import GameServerModel, WaitingPlayerModel
+        game_server = GameServerModel.objects.filter(state='waiting').first()
+        if game_server:
+
+            if game_server.firstPlayerId == -1:
+                player1 = WaitingPlayerModel.objects.first()
+                if player1:
+                    game_server.firstPlayerId = player1.player_id
+                    game_server.save()
+                    player1.delete()
+
+            if game_server.secondPlayerId == -1:
+                player2 = WaitingPlayerModel.objects.first()
+                if player2:
+                    game_server.firstPlayerId = player2.player_id
+                    game_server.save()
+                    player2.delete()
+
+            # Actualiser le game_server avec player info et change le game_server en full
+            if game_server.firstPlayerId != -1 and game_server.secondPlayerId != -1:
+                game_server.state = 'full'
+                game_server.save()
+        else:
+            GameServerModel.objects.create()
+        settings.IS_SEARCHING = False
