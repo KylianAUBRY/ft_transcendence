f2a5fdcb4e916b144c3f1f9fac5696bb2e38d666
Donovan Grandin <dgrandin@c2r1p1.42lehavre.fr>
2024-03-24 10:56:01 +0100

    correct CheckJoinGameView

--- src/back-end/users/utils.py
+++ src/back-end/users/utils.py
@@ -35,17 +35,21 @@ def updateUserOption(user_id, language, color, music, key1, key2, key3, key4):
 
     user.save()
 
+import logging
 from django.conf import settings
 def ManageGameQueue():
+    logger = logging.getLogger(__name__)
     if (settings.IS_SEARCHING == False):
         settings.IS_SEARCHING = True
         from . models import GameServerModel, WaitingPlayerModel
         game_server = GameServerModel.objects.filter(state='waiting').first()
         if game_server:
 
+            logger.info('Game server find: ' + game_server.serverId)
             if game_server.firstPlayerId == -1:
                 player1 = WaitingPlayerModel.objects.first()
                 if player1:
+                    logger.info('player1 find : ' + player1.player_id)
                     game_server.firstPlayerId = player1.player_id
                     game_server.save()
                     player1.delete()
@@ -53,14 +57,17 @@ def ManageGameQueue():
             if game_server.secondPlayerId == -1:
                 player2 = WaitingPlayerModel.objects.first()
                 if player2:
+                    logger.info('player2 find : ' + player2.player_id)
                     game_server.firstPlayerId = player2.player_id
                     game_server.save()
                     player2.delete()
 
             # Actualiser le game_server avec player info et change le game_server en full
             if game_server.firstPlayerId != -1 and game_server.secondPlayerId != -1:
+                logger.info('Game full')
                 game_server.state = 'full'
                 game_server.save()
         else:
+            logger.info('Game server model created')
             GameServerModel.objects.create()
         settings.IS_SEARCHING = False
