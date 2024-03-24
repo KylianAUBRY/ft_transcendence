75ea57c5fb2e681aab98f63a12e1365d2a691e10
Donovan Grandin <dgrandin@c2r1p1.42lehavre.fr>
2024-03-24 14:37:00 +0100

    test

--- src/back-end/Transcendence/settings.py
+++ src/back-end/Transcendence/settings.py
@@ -97,12 +97,14 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8000',
     'http://localhost:8000',
+    '*'
     'http://127.0.0.1:443',
     'http://localhost:443',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
+    '*'
     # Add other trusted origins if needed
 ]
 
