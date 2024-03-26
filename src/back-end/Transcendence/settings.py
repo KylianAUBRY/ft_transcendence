8bd06463191381832aacc4d63358f58b215e44be
dgrandin <dgrandin@42-lehavre.fr>
2024-03-26 23:43:53 +0100

    Pwd Hash

--- src/back-end/Transcendence/settings.py
+++ src/back-end/Transcendence/settings.py
@@ -225,6 +225,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media files
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
