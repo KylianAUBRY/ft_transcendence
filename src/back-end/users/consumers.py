f8d44f5cea9dbce5a742fb905de83f9dba79c355
Donovan Grandin <dgrandin@c2r7p3.42lehavre.fr>
2024-03-28 17:16:54 +0100

    test

--- src/back-end/users/consumers.py
+++ src/back-end/users/consumers.py
@@ -94,16 +94,17 @@ class GameRoom(AsyncWebsocketConsumer):
 
         player_to_disconnect = None
         serv_name = None
-        for name_serv, player in self.players.items():
-            logger.info("\n\n\n PLAYER : %s", str(player))
-            logger.info("\n\n\n PLAYER_ID : %s", player["idPlayer"])
-            logger.info("\n\n\n SCOPE_ID : %s", self.scope["user"].user_id)
+        for name_serv, playerr in self.players.items():
+            for player in playerr.items():
+                logger.info("\n\n\n PLAYER : %s", str(player))
+                logger.info("\n\n\n PLAYER_ID : %s", player["idPlayer"])
+                logger.info("\n\n\n SCOPE_ID : %s", self.scope["user"].user_id)
 
-            if player["idPlayer"] == self.scope["user"].user_id:
-                serv_name = player["serv"]
-                player_to_disconnect = player["idMatch"]
-                player["isDisconnect"] = True
-                break
+                if player["idPlayer"] == self.scope["user"].user_id:
+                    serv_name = player["serv"]
+                    player_to_disconnect = player["idMatch"]
+                    player["isDisconnect"] = True
+                    break
 
         logger.info("\n\n\n NAME_SERV : %s", name_serv)
         logger.info("\n\n\n PLAYER_TO_DISCONNECT : %s", player_to_disconnect)
