824dada9da4fa93a28f1545e47fa726f9943e994
Donovan Grandin <dgrandin@c2r7p3.42lehavre.fr>
2024-04-02 18:24:13 +0200

    Joinqueue fix

--- src/back-end/users/routing.py
+++ src/back-end/users/routing.py
@@ -1,7 +1,6 @@
 from django.urls import re_path 
 from . import consumers 
   
-websocket_urlpatterns = [ 
-    #re_path(r'ws/game/(?P<room_name>\w+)/(?P<auth_token>\w+)/$', consumers.GameRoom.as_asgi()),
-    re_path(r'ws/game/(?P<room_name>\w+)/(?P<token>\w+)/$', consumers.GameRoom.as_asgi()),
+websocket_urlpatterns = [
+    re_path(r'ws/game/(?P<room_name>\w+)/$', consumers.GameRoom.as_asgi()),
 ] 
