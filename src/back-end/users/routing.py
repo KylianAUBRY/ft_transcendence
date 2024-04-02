2baf52d380327608dadfab1cb52fe17c217548b0
Donovan Grandin <dgrandin@c2r7p3.42lehavre.fr>
2024-04-02 13:53:59 +0200

    bruh

--- src/back-end/users/routing.py
+++ src/back-end/users/routing.py
@@ -2,5 +2,6 @@ from django.urls import re_path
 from . import consumers 
   
 websocket_urlpatterns = [ 
+    #re_path(r'ws/game/(?P<room_name>\w+)/(?P<auth_token>\w+)/$', consumers.GameRoom.as_asgi()),
     re_path(r'ws/game/(?P<room_name>\w+)/$', consumers.GameRoom.as_asgi()),
 ] 
